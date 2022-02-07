7e08a6323e7a80dd653fc2ca30db1b2bfc076617
PierrePocheron <pierre.pocheron@gmail.com>
2022-02-07 16:38:26 +0100

    Feat : add view with class / function

--- authentification/views.py
+++ authentification/views.py
@@ -1,13 +1,56 @@
+import django
+from django.conf import settings
 from django.shortcuts import redirect, render
 from django.contrib.auth import login, logout,  authenticate
+from django.views.generic import View
+# from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from . import forms # import des fonctions login et authenticate
+
+# Vue Basé sur une classe
+# LoginRequiredMixin : restrint l'accès au site seulement aux utilsiateurs connectés
+class LoginPageView(View):
+    template_name = 'authentification/login.html'
+    form_class = forms.LoginForm
+
+    def get(self, request):
+        form = self.form_class()
+        message = ''
+        return render(request, self.template_name, context={'form': form, 'message': message})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+        message = 'Identifiants invalides.'
+        return render(request, self.template_name, context={'form': form, 'message': message})
+
 
 def logout_user(request):
     logout(request)
     return redirect('login')
 
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # auto-login user
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentification/signup.html', context={'form': form})
 
+
+# Pas utilisé
+# Vue basée sur une fonction
 def login_page(request):
     form = forms.LoginForm()
     message = ''
@@ -25,6 +68,3 @@ def login_page(request):
                 message = 'Identifiants invalides.'
     return render(
         request, 'authentification/login.html', context={'form': form, 'message': message})
-
-
-
